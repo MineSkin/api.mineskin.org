519627dfdff39c94f1615c466ae4a83305c1288d
inventivetalent <inventivetalent@inventivegames.de>
2017-10-25 19:26:35 +0200

    add /render/texture/:id

--- routes/render.js
+++ routes/render.js
@@ -33,24 +33,36 @@ module.exports = function (app) {
                 res.status(404).end();
             }
         })
+    });
+
+    // Helper route to avoid CORS issues
+    app.get("/render/texture/:id", function (req, res) {
+        Skin.findOne({id: req.params.id}, function (err, skin) {
+            if (err) return console.log(err);
+            if (skin) {
+                request(skin.url).pipe(res);
+            } else {
+                res.status(404).end();
+            }
+        })
     })
 
     function doRender(req, res, url, type, options) {
-       // request("http://tools.inventivetalent.org/skinrender/3d.php?headOnly=" + (type === "head") + "&user=" + url + options, function (err,response,body) {
-       //     console.log(body)
-       //      if (response.statusCode === 200) {
-       //          res.writeHead(200, {
-       //              "Content-Type": "image/png",
-       //              "Pragma": "public",
-       //              "Cache-Control": "max-age=604800",
-       //              "Expires": new Date(Date.now() + 604800).toUTCString()
-       //          });
-       //          console.log(response.pipe)
-       //          response.pipe(res)
-       //      } else {
-       //          res.status(response.statusCode).end();
-       //      }
-       //  })
+        // request("http://tools.inventivetalent.org/skinrender/3d.php?headOnly=" + (type === "head") + "&user=" + url + options, function (err,response,body) {
+        //     console.log(body)
+        //      if (response.statusCode === 200) {
+        //          res.writeHead(200, {
+        //              "Content-Type": "image/png",
+        //              "Pragma": "public",
+        //              "Cache-Control": "max-age=604800",
+        //              "Expires": new Date(Date.now() + 604800).toUTCString()
+        //          });
+        //          console.log(response.pipe)
+        //          response.pipe(res)
+        //      } else {
+        //          res.status(response.statusCode).end();
+        //      }
+        //  })
         request("http://tools.inventivetalent.org/skinrender/3d.php?headOnly=" + (type === "head") + "&user=" + url + options).pipe(res);
     }
 
