dade8c3a2cf0add68d83406f54bce099b25948d0
inventivetalent <mail@inventivetalent.org>
2019-03-03 15:40:28 +0100

    remove ecosystem config

--- ecosystem.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    apps : [{
-        name: 'MineSkin',
-        script: './index.js',
-        node_args: '--max_old_space_size=4096 --optimize_for_size --stack_size=4096',
-        cwd: '/home/inventivetalent/api.mineskin.org'
-    }]
-};
