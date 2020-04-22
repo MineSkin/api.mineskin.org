09c8bf47a74bc5a331516dabb8dee893690cf852
inventivetalent <mail@inventivetalent.org>
2020-04-22 08:32:39 +0200

    add hash test

---
+++ test/hashTest.js
@@ -0,0 +1,27 @@
+var hasha = require("hasha");
+
+var imageHash = function (path, callback) {
+    hasha.fromFile(path, {
+        algorithm: "sha1"
+    }).then(function (value) {
+        callback(null, value);
+    }).catch(function (reason) {
+        callback(reason, null);
+    })
+};
+
+imageHash("./images/a.png",function (err,hash) {
+    console.log("a: " + hash);
+});
+imageHash("./images/b.png",function (err,hash) {
+    console.log("b: " + hash);
+});
+imageHash("./images/c.png",function (err,hash) {
+    console.log("c: " + hash);
+});
+imageHash("./images/d.png",function (err,hash) {
+    console.log("d: " + hash);
+});
+imageHash("./images/e.png",function (err,hash) {
+    console.log("e: " + hash);
+});
