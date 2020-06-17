033b95e63b212059896871dc267543206fc6b0ae
inventivetalent <mail@inventivetalent.org>
2020-06-17 20:21:23 +0200

    start caching name/id requests

--- routes/util.js
+++ routes/util.js
@@ -8,9 +8,38 @@ module.exports = function (app) {
     var Skin = require("../db/schemas/skin").Skin;
     var Traffic = require("../db/schemas/traffic").Traffic;
 
+    var userByNameCache = {};
+    var nameByIdCache = {};
+
+    module.exports.cache = {
+        byName: userByNameCache,
+        byId: nameByIdCache
+    };
+
+    setInterval(function () {
+        for (var name in userByNameCache) {
+            if ((Date.now() / 1000) - userByNameCache[name].time > 240) {
+                delete userByNameCache[name];
+            }
+        }
+        for (var id in nameByIdCache) {
+            if ((Date.now() / 1000) - nameByIdCache[id].time > 240) {
+                delete userByNameCache[id];
+            }
+        }
+    }, 60000);
 
     app.get("/validate/user/:name", function (req, res) {
-        request("https://api.mojang.com/users/profiles/minecraft/" + req.params.name, function (err, response, body) {
+        if (req.params.name.length < 3) {
+            res.json({error: "invalid name"});
+            return;
+        }
+        var name = req.params.name.toLowerCase();
+        if (userByNameCache.hasOwnProperty(name)) {
+            res.json(userByNameCache[name]);
+            return;
+        }
+        request("https://api.mojang.com/users/profiles/minecraft/" + name, function (err, response, body) {
             console.log(("" + body).debug);
 
             var result = {
@@ -26,6 +55,9 @@ module.exports = function (app) {
                     result.valid = true;
                     result.uuid = body.id;
                     result.name = body.name;
+
+                    result.time = Date.now() / 1000;
+                    userByNameCache[body.name.toLowerCase()] = result;
                 }
             }
 
@@ -33,8 +65,17 @@ module.exports = function (app) {
         });
     });
 
-    app.get("/validate/currentUsername/:uuid",function (req,res) {
-        request("https://api.mojang.com/user/profiles/" + req.params.uuid + "/names", function (err, response, body) {
+    app.get("/validate/currentUsername/:uuid", function (req, res) {
+        if (req.params.uuid.length < 30) {
+            res.json({error: "invalid uuid"});
+            return;
+        }
+        var uuid = req.params.uuid.toLowerCase();
+        if (nameByIdCache.hasOwnProperty(uuid)) {
+            res.json(nameByIdCache[uuid]);
+            return;
+        }
+        request("https://api.mojang.com/user/profiles/" + uuid + "/names", function (err, response, body) {
             console.log(("" + body).debug);
 
             var result = {
@@ -47,6 +88,9 @@ module.exports = function (app) {
                 if (body) {
                     body = JSON.parse(body);
                     result.name = body[body.length - 1]["name"];
+
+                    result.time = Date.now() / 1000;
+                    nameByIdCache[uuid] = result;
                 }
             }
 
