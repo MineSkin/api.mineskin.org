41735bec3a19e39610a23e812ff0ccc7f4767010
inventivetalent <mail@inventivetalent.org>
2021-01-29 01:39:41 +0100

    try that

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: "mineskin",
         script: "dist/index.js",
-        args: ["--color", "--time"],
+        args: ["--color --time"],
         interpreter: "node@14.15.4",
         max_memory_restart: "300M"
     }]
