510aaf3228e1b672972e9d486cfb6c1debbbc926
inventivetalent <mail@inventivetalent.org>
2021-01-29 01:43:17 +0100

    ?

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
         name: "mineskin",
         script: "dist/index.js",
         args: ["--color", "--time"],
+        time: true,
         interpreter: "node@14.15.4",
         max_memory_restart: "300M"
     }]
