0fe234f99ff1e2df9c658a9d30fff006d5543f50
inventivetalent <mail@inventivetalent.org>
2021-02-03 16:54:33 +0100

    log

--- src/util/rateLimiters.ts
+++ src/util/rateLimiters.ts
@@ -1,6 +1,7 @@
-import { Request } from "express";
+import { Request, Response } from "express";
 import * as rateLimit from "express-rate-limit";
 import { getIp } from "./index";
+import { debug } from "./colors";
 
 
 function keyGenerator(req: Request): string {
@@ -11,5 +12,8 @@ export const generateLimiter = rateLimit({
     windowMs: 2 * 60 * 1000, // 2 minutes,
     max: 6,
     message: JSON.stringify({ error: "Too many requests" }),
-    keyGenerator: keyGenerator
+    keyGenerator: keyGenerator,
+    onLimitReached: (req: Request, res: Response) => {
+        console.log(debug(`${ getIp(req) } reached their rate limit`));
+    }
 });
