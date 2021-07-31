14d8f5fd0fb8507c626df0eb6a312e8f2f2bd621
inventivetalent <mail@inventivetalent.org>
2021-07-31 22:05:36 +0200

    Nicer Emails

--- src/util/Email.ts
+++ src/util/Email.ts
@@ -3,12 +3,13 @@ import * as Sentry from "@sentry/node";
 import { getConfig } from "../typings/Configs";
 import * as Mail from "nodemailer/lib/mailer";
 import { warn } from "./colors";
+import { Maybe } from "./index";
 
 const FROM = "noreply@mineskin.org";
 
 export class Email {
 
-    static async sendEmail(to: string, content: string, subject: string = "MineSkin Notification"): Promise<void> {
+    static async sendEmail(to: string, content: string, htmlContent: Maybe<string> = undefined, subject: string = "MineSkin Notification"): Promise<void> {
         const config = await getConfig();
         const transport = createTransport(config.email);
         transport.verify().then(() => {
@@ -16,7 +17,47 @@ export class Email {
                 from: `MineSkin ${ FROM }`,
                 to: to,
                 subject: subject,
-                text: content
+                text: content,
+                priority: "high",
+                encoding: "utf8",
+                html: `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>${ subject }</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        ${ htmlContent ? htmlContent : '<p>${content}</p>' }
+                
+        <div itemscope itemtype="http://schema.org/EmailMessage">
+          <meta itemprop="description" content="View Details on MineSkin">
+          <div itemprop="potentialAction" itemscope itemtype="http://schema.org/ViewAction">
+            <link itemprop="target" href="https://mineskin.org/account">
+            <meta itemprop="name" content="Login to Account">
+          </div>
+          <div itemprop="publisher" itemscope itemtype="http://schema.org/Organization">
+            <meta itemprop="name" content="MineSkin">
+            <link itemprop="url" href="https://mineskin.org/">
+          </div>
+        </div>
+        
+        <script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "EmailMessage",
+  "potentialAction": {
+    "@type": "ViewAction",
+    "target": "https://mineskin.org/account",
+    "name": "Login to Account"
+  },
+  "description": "View Details on MineSkin"
+}
+        </script>
+
+    </body>
+</html>
+`
             }
             transport.sendMail(message).catch(err => {
                 console.warn(warn(err))
--- src/util/Notifications.ts
+++ src/util/Notifications.ts
@@ -7,12 +7,16 @@ import { Email } from "./Email";
 
 export class Notifications {
 
-    protected static sendEmailAndDiscordMessage(account: IAccountDocument, message: (email: boolean) => string, publicMessage?: () => string) {
+    protected static sendEmailAndDiscord(account: IAccountDocument,
+                                         simpleMessage: (account: IAccountDocument, email: boolean) => string,
+                                         publicMessage: (account: IAccountDocument) => string,
+                                         htmlMessage: (account: IAccountDocument) => string,
+                                         subject: (account: IAccountDocument) => string) {
         if (account.discordUser && !account.discordMessageSent) {
             try {
-                Discord.sendDiscordDirectMessage(message(false), account.discordUser, () => {
+                Discord.sendDiscordDirectMessage(simpleMessage(account, false), account.discordUser, () => {
                     if (publicMessage) {
-                        Discord.postDiscordMessage(publicMessage(), OWNER_CHANNEL);
+                        Discord.postDiscordMessage(publicMessage(account), OWNER_CHANNEL);
                     }
                 });
 
@@ -23,7 +27,7 @@ export class Notifications {
         }
         if (account.sendEmails && account.email && !account.emailSent) {
             try {
-                Email.sendEmail(account.email, message(true));
+                Email.sendEmail(account.email, simpleMessage(account, true), htmlMessage(account), subject(account));
 
                 account.emailSent = true;
             } catch (e) {
@@ -33,7 +37,31 @@ export class Notifications {
     }
 
     protected static supportLink(email: boolean): string {
-        return "For further assistance feel free to ask " + (email ? "on https://yeleha.co/discord" : "in <#" + SUPPORT_CHANNEL + ">") + " 🙂"
+        return "For further assistance feel free to ask " + (email ? "on https://yeleha.co/discord" : "in <#" + SUPPORT_CHANNEL + ">") + " :)"
+    }
+
+    protected static accountInfo(acc: IAccountDocument, html: boolean): string {
+        if (html) {
+            return `
+<p style="margin-left: 20px">Affected Account: ${ acc.playername || acc.uuid } (${ acc.getEmail() })</p>
+<p style="margin-left: 20px">Account Type: ${ acc.getAccountType() }</p>
+<p style="margin-left: 20px">Last Error Code: ${ acc.lastErrorCode }</p>
+            `
+        } else {
+            return `
+  Affected Account: ${ acc.playername || acc.uuid } (${ acc.getEmail() })
+  Account Type: ${ acc.getAccountType() }
+  Last Error Code: ${ acc.lastErrorCode }
+            `
+        }
+    }
+
+    protected static trimmedAccountInfo(acc: IAccountDocument): string {
+        return "  Account UUID (trimmed): " + (acc.uuid || acc.playername || "").substr(0, 5) + "****\n";
+    }
+
+    protected static publicPrefix(acc: IAccountDocument): string {
+        return `Hey <@${ acc.discordUser }>! I tried to send a private message but couldn't reach you :(\n`
     }
 
     static notifyMissingCredentials(account: IAccountDocument): void {
@@ -45,22 +73,34 @@ export class Notifications {
             "  Account Added: " + new Date((account.timeAdded || 0) * 1000).toUTCString() + "\n" +
             "  Linked to <@" + account.discordUser + ">");
 
-        this.sendEmailAndDiscordMessage(account,
-            email => {
-                return "This is an automated notification that MineSkin lost access to one of your accounts and has been disabled\n" +
-                    "  Affected Account: " + (account.playername || account.uuid) + " (" + account.getEmail() + ")\n" +
-                    "  Account Type: " + account.getAccountType() + "\n" +
-                    "\n" +
-                    "The account won't be used for skin generation until the issues are resolved.\n" +
-                    "Please log back in to your account at https://mineskin.org/account\n" +
-                    this.supportLink(email);
-            },
-            () => {
-                return "Hey <@" + account.discordUser + ">! I tried to send a private message but couldn't reach you :(\n" +
-                    "MineSkin just lost access to one of your accounts (" + account.getAccountType() + ")\n" +
-                    "  Account UUID (trimmed): " + (account.uuid || account.playername || "").substr(0, 5) + "****\n" +
-                    "  Please log back in at https://mineskin.org/account\n"
-            })
+        this.sendEmailAndDiscord(account,
+            (acc, email) => `
+Hi there!
+This is an automated notification that MineSkin lost access to one of your accounts and has been disabled
+${ this.accountInfo(acc, false) }
+
+The account won't be used for skin generation until the issues are resolved.
+Please log back in to your account at https://mineskin.org/account
+${ account.getAccountType() === AccountType.MICROSOFT ? "You should also check https://account.live.com/Activity" : "" }
+${ this.supportLink(email) }
+            `,
+            acc => `
+${this.publicPrefix(acc)}
+MineSkin just lost access to one of your accounts (${ acc.getAccountType() })\n
+${ this.trimmedAccountInfo(acc) }
+Please log back in at https://mineskin.org/account\n
+            `,
+            acc => `
+<p><b>Hi there!</b></p>
+<p>This is an automated notification that MineSkin lost access to one of your accounts and has been disabled</p>
+${ this.accountInfo(acc, true) }
+<p></p>
+<p>The account won't be used for skin generation until the issues are resolved.</p>
+<p>Please log back in to your account at <a href="https://mineskin.org/account">mineskin.org/account</a></p>
+${ account.getAccountType() === AccountType.MICROSOFT ? "<p>You should also check <a href='https://account.live.com/Activity'>account.live.com/Activity</a></p>" : "" }
+<p>${ this.supportLink(true) }</p>
+            `,
+            acc => `MineSkin Notification ${ acc.playername || acc.uuid }`)
     }
 
     static notifyLoginFailed(account: IAccountDocument, err: MineSkinError): void {
@@ -74,24 +114,34 @@ export class Notifications {
             "  Account Added: " + new Date((account.timeAdded || 0) * 1000).toUTCString() + "\n" +
             "  Linked to <@" + account.discordUser + ">");
 
-        this.sendEmailAndDiscordMessage(account,
-            email => {
-                return "Hi there!\n" +
-                    "This is an automated notification that MineSkin just failed to login to your account\n" +
-                    "  Affected Account: " + (account.playername || account.uuid) + " (" + account.getEmail() + ")\n" +
-                    "  Account Type: " + account.getAccountType() + "\n" +
-                    "\n" +
-                    "The account won't be used for skin generation until the issues are resolved.\n" +
-                    "Please try to login at https://www.minecraft.net/login and check if there are any issues with your account and then log back in to your account at https://mineskin.org/account\n" +
-                    (account.getAccountType() === AccountType.MICROSOFT ? "You should also check https://account.live.com/Activity\n" : "") +
-                    this.supportLink(email);
-            },
-            () => {
-                return "Hey <@" + account.discordUser + ">! I tried to send a private message but couldn't reach you :(\n" +
-                    "MineSkin just failed to login to one of your accounts (" + account.getAccountType() + ")\n" +
-                    "  Account UUID (trimmed): " + (account.uuid || account.playername || "").substr(0, 5) + "****\n" +
-                    "  Please check your account for issues and log back in at https://mineskin.org/account\n"
-            })
+        this.sendEmailAndDiscord(account,
+            (acc, email) => `
+Hi there!
+This is an automated notification that MineSkin just failed to login to your account
+${ this.accountInfo(acc, false) }
+
+The account won't be used for skin generation until the issues are resolved
+Please try to login at https://www.minecraft.net/login and check if there are any issues with your account and then log back in to your account at https://mineskin.org/account
+${ account.getAccountType() === AccountType.MICROSOFT ? "You should also check https://account.live.com/Activity" : "" }
+${ this.supportLink(email) }
+            `,
+            acc => `
+${this.publicPrefix(acc)}
+MineSkin just failed to login to one of your accounts (${ acc.getAccountType() })\n
+${ this.trimmedAccountInfo(acc) }
+Please log back in at https://mineskin.org/account\n
+            `,
+            acc => `
+<p><b>Hi there!</b></p>
+<p>This is an automated notification that MineSkin just failed to login to your account</p>
+${ this.accountInfo(acc, true) }
+<p></p>
+<p>The account won't be used for skin generation until the issues are resolved.</p>
+<p>Please try to login at <a href="https://www.minecraft.net/login">minecraft.net</a> and check if there are any issues with your account and then log back in to your account at <a href="https://mineskin.org/account">mineskin.org/account</a></p>
+${ account.getAccountType() === AccountType.MICROSOFT ? "<p>You should also check <a href='https://account.live.com/Activity'>account.live.com/Activity</a></p>" : "" }
+<p>${ this.supportLink(true) }</p>
+            `,
+            acc => `MineSkin Notification ${ acc.playername || acc.uuid }`)
     }
 
 
@@ -106,24 +156,34 @@ export class Notifications {
             "  Account Added: " + new Date((account.timeAdded || 0) * 1000).toUTCString() + "\n" +
             "  Linked to <@" + account.discordUser + ">");
 
-        this.sendEmailAndDiscordMessage(account,
-            email => {
-                return "Hi there!\n" +
-                    "This is an automated notification that one of your MineSkin accounts been disabled since it failed to properly generate skin data recently.\n" +
-                    "  Affected Account: " + (account.playername || account.uuid) + " (" + account.getEmail() + ")\n" +
-                    "  Account Type: " + account.getAccountType() + "\n" +
-                    "  Last Error Code:  " + account.lastErrorCode + "\n" +
-                    "\n" +
-                    "The account won't be used for skin generation until the issues are resolved.\n" +
-                    "Please make sure the configured credentials & security questions are correct at https://mineskin.org/account\n" +
-                    this.supportLink(email);
-            },
-            () => {
-                return "Hey <@" + account.discordUser + ">! I tried to send a private message but couldn't reach you :(\n" +
-                    "One of your accounts (" + account.getAccountType() + ") was just disabled since it failed to properly generate skin data recently.\n" +
-                    "  Account UUID (trimmed): " + (account.uuid || account.playername || "").substr(0, 5) + "****\n" +
-                    "  Please log back in at https://mineskin.org/account\n"
-            })
+        this.sendEmailAndDiscord(account,
+            (acc, email) => `
+Hi there!
+This is an automated notification that one of your MineSkin accounts been disabled since it failed to properly generate skin data recently.
+${ this.accountInfo(acc, false) }
+
+The account won't be used for skin generation until the issues are resolved.
+Please make sure the configured credentials & security questions are correct at https://mineskin.org/account
+${ account.getAccountType() === AccountType.MICROSOFT ? "You should also check https://account.live.com/Activity" : "" }
+${ this.supportLink(email) }
+            `,
+            acc => `
+${this.publicPrefix(acc)}
+One of your accounts (${ acc.getAccountType() }) was just disabled since it failed to properly generate skin data recently. 
+${ this.trimmedAccountInfo(acc) }
+Please log back in at https://mineskin.org/account
+            `,
+            acc => `
+<p><b>Hi there!</b></p>
+<p>This is an automated notification that one of your MineSkin accounts been disabled since it failed to properly generate skin data recently.</p>
+${ this.accountInfo(acc, true) }
+<p></p>
+<p>The account won't be used for skin generation until the issues are resolved.</p>
+<p>Please make sure the configured credentials & security questions are correct at  <a href="https://mineskin.org/account">mineskin.org/account</a></p>
+${ account.getAccountType() === AccountType.MICROSOFT ? "<p>You should also check <a href='https://account.live.com/Activity'>account.live.com/Activity</a></p>" : "" }
+<p>${ this.supportLink(true) }</p>
+            `,
+            acc => `MineSkin Notification ${ acc.playername || acc.uuid }`)
     }
 
 }
