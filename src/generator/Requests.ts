09b15d265def69a82eb3a668b0f7357821dfb39d
inventivetalent <mail@inventivetalent.org>
2022-01-27 19:29:36 +0100

    .

--- src/generator/Requests.ts
+++ src/generator/Requests.ts
@@ -59,7 +59,7 @@ export class Requests {
         = new JobQueue<AxiosRequestConfig, AxiosResponse>((request: AxiosRequestConfig) => Requests.runAxiosRequest(request, Requests.liveLoginInstance), Time.millis(200), 1);
 
     protected static readonly minecraftServicesSkinRequestThrottle: Throttle<AxiosRequestConfig, AxiosResponse>
-        = new Throttle<AxiosRequestConfig, AxiosResponse>(Time.seconds(3), request => Requests.runAxiosRequest(request, Requests.minecraftServicesInstance));
+        = new Throttle<AxiosRequestConfig, AxiosResponse>(Time.seconds(3), request => Requests.runAxiosRequest(request, Requests.minecraftServicesInstance)); // 2s is too fast already...
 
     protected static metricsCollector = setInterval(async () => {
         const config = await getConfig();
