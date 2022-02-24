d45c89676e414cbad4fb768bb700c9b4ae993dae
inventivetalent <mail@inventivetalent.org>
2022-02-24 14:13:10 +0100

    .

--- src/generator/Generator.ts
+++ src/generator/Generator.ts
@@ -40,7 +40,7 @@ import { FilterQuery } from "mongoose";
 
 
 // minimum delay for accounts to be used
-export const MIN_ACCOUNT_DELAY = 40;
+export const MIN_ACCOUNT_DELAY = 20;
 
 const MAX_ID_TRIES = 10;
 
