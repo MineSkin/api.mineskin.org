e24772a70e7c6046827e2b0018959849ed2584a2
inventivetalent <mail@inventivetalent.org>
2022-03-30 12:52:35 +0200

    .

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,9 +1,11 @@
 import { Application } from "express";
 import { MineSkinConfig } from "../typings/Configs";
-import { corsWithCredentialsMiddleware } from "../util";
+import { corsWithCredentialsMiddleware, getIp } from "../util";
 import jwt from "jsonwebtoken";
 import { LoginTicket, OAuth2Client } from "google-auth-library";
+import { v4 as randomUuid } from "uuid";
 import * as fs from "fs";
+import { Caching } from "../generator/Caching";
 
 export const register = (app: Application, config: MineSkinConfig) => {
 
@@ -21,7 +23,36 @@ export const register = (app: Application, config: MineSkinConfig) => {
     //     }
     // }))
 
+    app.get("/account/google/frame", async (req, res) => {
+        const nonce = randomUuid().replace(/-/g, '');
+        Caching.putLoginNonce(getIp(req), nonce);
+        res.send(`
+                <div class="g_id_signin"
+                     data-server="${ config.server }"
+                     data-nonce="${ nonce }"
+                     data-locale="en"
+                     data-logo_alignment="left"
+                     data-shape="pill"
+                     data-size="large"
+                     data-text="continue_with"
+                     data-theme="filled_blue"
+                     data-type="standard">
+                </div>
+                <div data-auto_select="true"
+                     data-client_id="352641379376-54jd29mpaorrk7bdvqh4qlll4a4n5g2b.apps.googleusercontent.com"
+                     data-context="use"
+                     data-login_uri="https://${ config.server }.api.mineskin.org/account/google/callback"
+                     data-todo="url" data-ux_mode="popup"
+                     id="g_id_onload">
+                </div>
+                <script async defer src="https://accounts.google.com/gsi/client"></script>
+        `)
+    })
+
     app.post("/account/google/callback", async (req, res) => {
+        console.log(req.cookies);
+        console.log(req.body)
+
         const csrfCookie = req.cookies['g_csrf_token'];
         if (!csrfCookie || csrfCookie.length <= 0) {
             res.status(400);
@@ -38,12 +69,12 @@ export const register = (app: Application, config: MineSkinConfig) => {
         }
 
         let ticket: LoginTicket;
-        try{
+        try {
             ticket = await googleClient.verifyIdToken({
                 idToken: req.body['credential'],
                 audience: config.google.id
             });
-        }catch (e ){
+        } catch (e) {
             console.warn(e);
             res.status(400);
             return;
