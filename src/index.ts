d21793921f58cd8fb99a120c1c2e31e38253abed
inventivetalent <mail@inventivetalent.org>
2022-12-11 14:28:16 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -346,8 +346,9 @@ async function init() {
 
     setInterval(() => {
         try {
+            if (Generator.lastSave == 0) return;
             let diff = Date.now() - Generator.lastSave;
-            if (diff > 60 * 1000 * 2) {
+            if (diff > 60 * 1000 * 5) {
                 Discord.postDiscordMessage(config.server + " hasn't saved any new skins for " + (diff / 1000 / 60) + "m");
             }
         } catch (e) {
