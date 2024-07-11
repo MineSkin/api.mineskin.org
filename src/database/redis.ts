ae8fb9ad8b2945eac7982e124a320462aff35ec6
Haylee Schäfer <haylee@inventivetalent.org>
2024-07-11 23:47:20 +0200

    keep stats longer

--- src/database/redis.ts
+++ src/database/redis.ts
@@ -47,11 +47,11 @@ function trackRedisGenerated0(trans: any, newOrDup: string, prefix: string) {
     trans?.incr(`${ prefix }:alltime:${ newOrDup }`);
 
     trans?.incr(`${ prefix }:${ date.getFullYear() }:${ newOrDup }`);
-    trans?.expire(`${ prefix }:${ date.getFullYear() }:${ newOrDup }`, ONE_YEAR_SECONDS * 2);
+    trans?.expire(`${ prefix }:${ date.getFullYear() }:${ newOrDup }`, ONE_YEAR_SECONDS * 5);
 
     trans?.incr(`${ prefix }:${ date.getFullYear() }:${ date.getMonth() + 1 }:${ newOrDup }`);
-    trans?.expire(`${ prefix }:${ date.getFullYear() }:${ date.getMonth() + 1 }:${ newOrDup }`, ONE_YEAR_SECONDS);
+    trans?.expire(`${ prefix }:${ date.getFullYear() }:${ date.getMonth() + 1 }:${ newOrDup }`, ONE_YEAR_SECONDS * 2);
 
     trans?.incr(`${ prefix }:${ date.getFullYear() }:${ date.getMonth() + 1 }:${ date.getDate() }:${ newOrDup }`);
-    trans?.expire(`${ prefix }:${ date.getFullYear() }:${ date.getMonth() + 1 }:${ date.getDate() }:${ newOrDup }`, ONE_MONTH_SECONDS);
+    trans?.expire(`${ prefix }:${ date.getFullYear() }:${ date.getMonth() + 1 }:${ date.getDate() }:${ newOrDup }`, ONE_MONTH_SECONDS * 3);
 }
