bc2bb61fe6930659c51e9bf724d26c35ecc0630d
Haylee Schäfer <haylee@inventivetalent.org>
2024-07-14 21:55:02 +0200

    add purple heart cape

--- src/util/Capes.ts
+++ src/util/Capes.ts
@@ -1,5 +1,6 @@
 export enum Capes {
     VANILLA = '7412868d-7be3-429f-b629-ed5074d2455c',
     MIGRATOR = '5af20372-79e0-4e1f-80f8-6bd8e3135995',
-    ANNIVERSARY_15 = 'ef9e95b6-48a3-4fd7-93d4-7e7d9448d2f1'
+    ANNIVERSARY_15 = 'ef9e95b6-48a3-4fd7-93d4-7e7d9448d2f1',
+    PURPLE_HEART = 'e7fcd100-d7dd-4c6c-a5f2-34bad18a3869'
 }
