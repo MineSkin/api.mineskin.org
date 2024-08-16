ea64d37c2e349d379650983ba615046e59d1ed26
Haylee Schäfer <haylee@inventivetalent.org>
2024-08-16 19:17:22 +0200

    adjust duplicate skin delay

--- src/generator/Caching.ts
+++ src/generator/Caching.ts
@@ -163,7 +163,7 @@ export class Caching {
     //// DATABASE
 
     protected static readonly trafficByIpCache: AsyncLoadingCache<string, Date> = Caches.builder()
-        .expireAfterWrite(Time.seconds(5))
+        .expireAfterWrite(Time.seconds(1))
         .expirationInterval(Time.seconds(1))
         .buildAsync<string, Date>(async (ip) => {
             const traffic = await Traffic.findForIp(ip);
@@ -171,7 +171,7 @@ export class Caching {
         });
 
     protected static readonly trafficByKeyCache: AsyncLoadingCache<string, Date> = Caches.builder()
-        .expireAfterWrite(Time.seconds(5))
+        .expireAfterWrite(Time.seconds(1))
         .expirationInterval(Time.seconds(1))
         .buildAsync<string, Date>(async (key) => {
             const traffic = await Traffic.findForKey(key);
