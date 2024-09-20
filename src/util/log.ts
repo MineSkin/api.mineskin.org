bbc9d74b7afa13310511119322748d1f96868103
Haylee Schäfer <haylee@inventivetalent.org>
2024-09-20 12:00:47 +0200

    add separate http logger

--- src/util/log.ts
+++ src/util/log.ts
@@ -70,6 +70,40 @@ if (logtail) {
 }
 
 
+const httpLogRotate: DailyRotateFile = new DailyRotateFile({
+    level: 'debug',
+    filename: 'logs/mineskin-http-%DATE%.log',
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '7d',
+    createSymlink: true,
+    symlinkName: 'current-http.log'
+});
+
+httpLogRotate.on('error', error => {
+    console.warn("logrotate failed", error);
+    Sentry.captureException(error);
+});
+
+httpLogRotate.on('rotate', (oldFilename, newFilename) => {
+    console.info(`Rotated log file from ${oldFilename} to ${newFilename}`);
+});
+
+export const httpLogger = winston.createLogger({
+    level: 'http',
+    format: format.combine(
+        format.timestamp({format: 'YYYY-MM-DD HH:mm:ss.SSS'})
+    ),
+    transports: [
+        httpLogRotate,
+        new winston.transports.Console({
+            level: 'debug',
+        })
+    ],
+});
+
+
 (()=>{
     console.log = (...args: any[]) => {
         logger.info(args.join(' '));
