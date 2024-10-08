39bfa25834ca688e8b8978d5fc4e0b725a4ed035
Haylee Schäfer <haylee@inventivetalent.org>
2024-10-08 21:52:28 +0200

    add /skins/uuid

--- src/models/v2/skins.ts
+++ src/models/v2/skins.ts
@@ -2,10 +2,13 @@ import { MineSkinV2Request } from "../../routes/v2/types";
 import { SkinService } from "@mineskin/generator";
 import { Response } from "express";
 import { ListReqQuery } from "../../runtype/ListReq";
-import { ISkin2Document, Skin2 } from "@mineskin/database";
+import { ISkin2Document, isPopulatedSkin2Document, Skin2 } from "@mineskin/database";
 import { RootFilterQuery } from "mongoose";
-import { SkinVisibility2 } from "@mineskin/types";
+import { MineSkinError, SkinVisibility2 } from "@mineskin/types";
 import { ListedSkin, V2SkinListResponseBody } from "../../typings/v2/V2SkinListResponseBody";
+import { V2SkinResponse } from "../../typings/v2/V2SkinResponse";
+import { UUID } from "../../runtype/misc";
+import { V2GenerateHandler } from "../../generator/v2/V2GenerateHandler";
 
 export async function v2SkinList(req: MineSkinV2Request, res: Response<V2SkinListResponseBody>): Promise<V2SkinListResponseBody> {
     const {
@@ -40,6 +43,20 @@ export async function v2SkinList(req: MineSkinV2Request, res: Response<V2SkinLis
         success: true,
         skins: skins.map(skinToSimpleJson)
     }
+}
+
+export async function v2GetSkin(req: MineSkinV2Request, res: Response<V2SkinResponse>): Promise<V2SkinResponse> {
+    const uuid = UUID.check(req.params.uuid);
+
+    const skin = await Skin2.findForUuid(uuid);
+    if (!skin || !isPopulatedSkin2Document(skin)) {
+        throw new MineSkinError('skin_not_found', 'Skin not found', {httpCode: 404});
+    }
+
+    return {
+        success: true,
+        skin: V2GenerateHandler.skinToJson(skin)
+    };
 }
 
 function skinToSimpleJson(skin: ISkin2Document): ListedSkin {
--- src/routes/v2/skins.ts
+++ src/routes/v2/skins.ts
@@ -1,15 +1,21 @@
 import { Response, Router } from "express";
-import { v2SkinList } from "../../models/v2/skins";
+import { v2GetSkin, v2SkinList } from "../../models/v2/skins";
 import { v2Router } from "./router";
 import { v2ErrorHandler } from "../../models/v2/generate";
 import expressAsyncHandler from "express-async-handler";
 import { MineSkinV2Request } from "./types";
 import { V2SkinListResponseBody } from "../../typings/v2/V2SkinListResponseBody";
+import { V2SkinResponse } from "../../typings/v2/V2SkinResponse";
 
 export const v2SkinsRouter: Router = v2Router();
 
 v2SkinsRouter.get("/", expressAsyncHandler(async (req: MineSkinV2Request, res: Response<V2SkinListResponseBody>) => {
     const result = await v2SkinList(req, res);
+    res.json(result);
+}));
+
+v2SkinsRouter.get("/:uuid", expressAsyncHandler(async (req: MineSkinV2Request, res: Response<V2SkinResponse>) => {
+    const result = await v2GetSkin(req, res);
     res.json(result);
 }));
 
