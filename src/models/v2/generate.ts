32075026ad804cb9f0f926f7084fa52891a6a960
Haylee Schäfer <haylee@inventivetalent.org>
2024-10-09 12:12:07 +0200

    redis user+pass

--- src/models/v2/generate.ts
+++ src/models/v2/generate.ts
@@ -42,7 +42,10 @@ const upload = multer({
 const client = new GeneratorClient({
     connection: {
         host: process.env.REDIS_HOST,
-        port: parseInt(process.env.REDIS_PORT!)
+        port: parseInt(process.env.REDIS_PORT!),
+        db: parseInt(process.env.REDIS_DB!) || 0,
+        username: process.env.REDIS_USERNAME,
+        password: process.env.REDIS_PASSWORD
     },
     blockingConnection: false
 });
