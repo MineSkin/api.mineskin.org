bb052accfe1f899ca739957c69d53becae601b29
Haylee Schäfer <haylee@inventivetalent.org>
2024-10-11 14:41:23 +0200

    fix

--- src/routes/v2/delay.ts
+++ src/routes/v2/delay.ts
@@ -2,6 +2,9 @@ import { Router } from "express";
 import { v2Router } from "./router";
 import { MineSkinV2Request } from "./types";
 import { v2ErrorHandler } from "../../middleware/error";
+import expressAsyncHandler from "express-async-handler";
+import { v2MeRouter } from "./me";
+import { v2GetDelay } from "../../models/v2/delay";
 
 export const v2DelayRouter: Router = v2Router();
 
@@ -9,5 +12,9 @@ v2DelayRouter.use((req: MineSkinV2Request, res, next) => {
     res.header("Cache-Control", "private, no-store, max-age=1");
     next();
 });
+
+v2MeRouter.get("/", expressAsyncHandler(async (req: MineSkinV2Request, res) => {
+    await v2GetDelay(req, res);
+}));
 
 v2DelayRouter.use("/", v2ErrorHandler);
