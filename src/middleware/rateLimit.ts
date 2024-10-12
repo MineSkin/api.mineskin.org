a7db821f87bf69fff7fb2115504092b7ab52282d
Haylee Schäfer <haylee@inventivetalent.org>
2024-10-12 13:59:37 +0200

    add rate limit headers

--- src/middleware/rateLimit.ts
+++ src/middleware/rateLimit.ts
@@ -9,9 +9,12 @@ export const rateLimitMiddleware = async (req: GenerateV2Request, res: Response,
 
     // check rate limit
     const trafficService = TrafficService.getInstance();
-    if(req.client.useDelayRateLimit()) {
+    if (req.client.useDelayRateLimit()) {
         req.nextRequest = await trafficService.getNextRequest(req.clientInfo);
         req.minDelay = await trafficService.getMinDelaySeconds(req.clientInfo, req.apiKey) * 1000;
+        res.header('X-RateLimit-Delay', `${ req.minDelay }`);
+        res.header('X-RateLimit-NextRequest', `${ req.nextRequest }`);
+        res.header('Retry-After', `${ Math.round((req.nextRequest - Date.now()) / 1000) }`);
         if (req.nextRequest > req.clientInfo.time) {
             throw new GeneratorError('rate_limit', `request too soon, next request in ${ ((Math.round(req.nextRequest - Date.now()) / 100) * 100) }ms`, {httpCode: 429});
         }
@@ -19,6 +22,8 @@ export const rateLimitMiddleware = async (req: GenerateV2Request, res: Response,
     if (req.client.usePerMinuteRateLimit()) {
         const counter = await trafficService.getCounter(req.clientInfo);
         const limit = req.client.getPerMinuteRateLimit();
+        res.header('X-RateLimit-Limit', `${ limit }`);
+        res.header('X-RateLimit-Remaining', `${ limit - counter }`);
         if (counter > limit) {
             throw new GeneratorError('rate_limit', `rate limit exceeded, ${ counter } > ${ limit }`, {httpCode: 429});
         }
--- src/routes/v2/test.ts
+++ src/routes/v2/test.ts
@@ -1,31 +1,38 @@
 import { v2Router } from "./router";
-import { BillingService } from "@mineskin/generator";
+import { BillingService, TrafficService } from "@mineskin/generator";
 import { MineSkinV2Request } from "./types";
 import { Response } from "express";
 import { v2ErrorHandler } from "../../middleware/error";
+import { rateLimitMiddleware } from "../../middleware/rateLimit";
 
 export const v2TestRouter = v2Router();
 
-v2TestRouter.get("/client",async (req: MineSkinV2Request, res: Response) => {
+v2TestRouter.get("/client", async (req: MineSkinV2Request, res: Response) => {
     const client = req.clientInfo;
     res.json({client});
 });
 
-v2TestRouter.get("/apikey",async (req: MineSkinV2Request, res: Response) => {
+v2TestRouter.get("/apikey", async (req: MineSkinV2Request, res: Response) => {
     const key = req.apiKey;
     res.json({key});
 });
 
-v2TestRouter.get("/billing/credits",async (req: MineSkinV2Request, res: Response) => {
+v2TestRouter.get("/billing/credits", async (req: MineSkinV2Request, res: Response) => {
     const billingService = BillingService.getInstance();
     const credit = await billingService.getClientCredits(req.clientInfo!)
-    res.json({ credit });
+    res.json({credit});
 });
 
-v2TestRouter.post("/billing/simulate-new-skin",async (req: MineSkinV2Request, res: Response) => {
+v2TestRouter.post("/billing/simulate-new-skin", async (req: MineSkinV2Request, res: Response) => {
     const billingService = BillingService.getInstance();
     await billingService.trackNewSkin(req.clientInfo!);
-    res.json({ success: true });
+    res.json({success: true});
+});
+
+v2TestRouter.post("/generate/rate-limit", rateLimitMiddleware, async (req: MineSkinV2Request, res: Response) => {
+    const trafficService = TrafficService.getInstance();
+    const count = await trafficService.incRequest(req.clientInfo!);
+    res.json({count});
 });
 
 v2TestRouter.use("/", v2ErrorHandler);
