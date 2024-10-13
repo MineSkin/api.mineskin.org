dfcabf261b4ee0c5c88d73729b6a18022b15e7ac
Haylee Schäfer <haylee@inventivetalent.org>
2024-10-13 11:34:39 +0200

    cors

--- src/routes/v2/router.ts
+++ src/routes/v2/router.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { mineSkinV2InitialMiddleware } from "../../middleware/combined";
+import { corsMiddleware } from "../../util";
 
 export function v2Router() {
     const router: Router = Router();
+
+    router.use("/", corsMiddleware); //TODO: should ues the cors plugin from express
 
     router.use("/", mineSkinV2InitialMiddleware);
 
