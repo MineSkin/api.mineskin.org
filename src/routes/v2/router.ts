38b90403eef997c397c08a62850ae3c8899d923b
Haylee Schäfer <haylee@inventivetalent.org>
2024-10-15 11:00:59 +0200

    don't use multiple cors ig

--- src/routes/v2/router.ts
+++ src/routes/v2/router.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import { mineSkinV2InitialMiddleware } from "../../middleware/combined";
-import { mineskinOnlyCors } from "../../middleware/cors";
 import helmet from "helmet";
 
 export function v2Router() {
     const router: Router = Router();
 
-    router.use(mineskinOnlyCors);
     router.use(helmet())
 
     router.use(mineSkinV2InitialMiddleware);
