ee7f4ecf3688dbf54da8915f6e376bda34831915
Haylee Schäfer <haylee@inventivetalent.org>
2024-11-21 11:18:42 +0100

    log full id

--- src/middleware/client.ts
+++ src/middleware/client.ts
@@ -51,7 +51,7 @@ export const clientFinalMiddleware = async (req: MineSkinV2Request, res: Respons
 export const finalizeRequestClient = async (req: MineSkinV2Request, res: Response) => {
     req.clientInfo = await req.client.asClientInfo(req);
     Sentry.setUser({
-        username: `${ req.clientInfo.key?.substring(0, 16) } ${ req.clientInfo.agent }`,
+        username: `${ req.clientInfo.key } ${ req.clientInfo.agent }`,
         ip_address: `${ req.clientInfo.ip }`
     });
 }
