c5d9f210892f15e00cf112f9739b995fdf2975c8
Haylee Schäfer <haylee@inventivetalent.org>
2024-12-13 11:13:42 +0100

    add popular skins sitemap

--- src/routes/v2/sitemaps.ts
+++ src/routes/v2/sitemaps.ts
@@ -12,13 +12,44 @@ router.get("/web-skins.xml", expressAsyncHandler(async (req: MineSkinV2Request, 
     res.header('Cache-Control', 'public, max-age=3600');
     res.header('Content-Type', 'application/xml');
 
-     const query: RootFilterQuery<ISkin2Document> = {
+    const query: RootFilterQuery<ISkin2Document> = {
         'meta.visibility': SkinVisibility2.PUBLIC
     };
-     const skins = await Skin2.find(query)
+    const skins = await Skin2.find(query)
         .limit(2048)
         .select('uuid meta updatedAt')
         .sort({_id: -1})
+        .exec();
+
+    let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
+    xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+    for (let skin of skins) {
+        xml += '  <url>\n';
+        xml += `    <loc>https://mineskin.org/skins/${ skin.uuid }</loc>\n`;
+        xml += `    <lastmod>${ skin.updatedAt.toISOString() }</lastmod>\n`;
+        xml += '  </url>\n';
+    }
+    xml += '</urlset>\n';
+
+    res.send(xml);
+}));
+
+router.get("/web-skins-popular-24h.xml", expressAsyncHandler(async (req: MineSkinV2Request, res: Response) => {
+    res.header('Cache-Control', 'public, max-age=3600');
+    res.header('Content-Type', 'application/xml');
+
+    const oneDayAgo = new Date();
+    oneDayAgo.setDate(oneDayAgo.getDate() - 1);
+
+    const query: RootFilterQuery<ISkin2Document> = {
+        'meta.visibility': SkinVisibility2.PUBLIC,
+        'interaction.views': {$gt: 5},
+        createdAt: {$gte: oneDayAgo}
+    };
+    const skins = await Skin2.find(query)
+        .limit(2048)
+        .select('uuid meta updatedAt')
+        .sort({_id: -1, 'interaction.views': -1})
         .exec();
 
     let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
