a5e6c136caad2219dab47c76901a2425a306c098
Haylee Schäfer <haylee@inventivetalent.org>
2025-01-18 14:14:39 +0100

    log

--- src/generator/Balancer.ts
+++ src/generator/Balancer.ts
@@ -147,6 +147,7 @@ export class Balancer {
                 if (madeChanges && newOriginConfig.length === currentOriginConfig.length) {
                     console.log(JSON.stringify(newOriginConfig));
                     const updateResponse = await this.patchPoolOrigins(config, poolId, newOriginConfig);
+                    console.log(updateResponse);
                     if (!updateResponse.success) {
                         console.warn(warn("failed to update pool config for " + poolId));
                         console.warn(updateResponse);
