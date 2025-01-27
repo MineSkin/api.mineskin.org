ac0710ef1ff0f28644f81366fefae8b8fffd5d49
Haylee Schäfer <haylee@inventivetalent.org>
2025-01-27 16:07:22 +0100

    fix it here too

--- src/middleware/apikey.ts
+++ src/middleware/apikey.ts
@@ -59,7 +59,7 @@ export const verifyApiKey = async (req: MineSkinV2Request, res: Response) => {
             // Either a server IP or a client origin, not both
             if (key.allowedIps && key.allowedIps.length > 0) {
                 const ip = getIp(req);
-                if (!ip || key.allowedIps.includes(ip.trim())) {
+                if (!ip || !key.allowedIps.includes(ip.trim())) {
                     console.log(debug(`Client ${ ip } not allowed`));
                     throw new MineSkinError("invalid_api_key", "Client not allowed", {httpCode: 403});
                 }
