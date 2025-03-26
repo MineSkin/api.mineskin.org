db6f83c6abd1c8bf0e32434811fa92cfbe59f5d4
Haylee Schäfer <haylee@inventivetalent.org>
2025-03-26 10:23:39 +0100

    change default grants to ignore credits

--- src/middleware/grants.ts
+++ src/middleware/grants.ts
@@ -18,30 +18,40 @@ export const verifyGrants = async (req: MineSkinV2Request, res: Response) => {
         const hasApiKey = req.client.hasApiKey();
         const hasUser = req.client.hasUser();
 
+        /*
         const credits = await req.client.getCredits();
         const creditType = credits && credits.isValid() && credits.balance > 0 ? credits.type : undefined;
+         */
 
-        const grants = await getDefaultGrants(hasApiKey, hasUser, creditType);
+        const grants = await getDefaultGrants(hasApiKey, hasUser, undefined);
         req.client.grants = {...grants, ...req.client.grants};
     })
 }
 
 async function getDefaultGrants(hasApiKey: boolean, hasUser: boolean, creditType: CreditType | undefined) {
     const flags = container.get<IFlagProvider>(CoreTypes.FlagProvider);
-    if (!hasApiKey) {
+    if (!hasApiKey && !hasUser) {
         // no api key, can't check credits -> use default
         return JSON.parse(await flags.getValue('generator.default_grants.base'));
     }
-    if (!creditType) {
+    if (hasApiKey) {
         // has api key, but no credits -> use default api key delay
         return JSON.parse(await flags.getValue('generator.default_grants.apikey'));
     }
+    if (hasUser) {
+        // has user, but no credits -> use default user delay
+        return JSON.parse(await flags.getValue('generator.default_grants.user'));
+    }
 
+    /*
     if (creditType === CreditType.PAID) {
         // has paid credits -> use default paid credits
         return JSON.parse(await flags.getValue('generator.default_grants.credits.paid'));
     }
 
     // fallback to default free credits
-    return JSON.parse(await flags.getValue('generator.default_grants.credits.free'));
+    return JSON.parse(await flags.getValue('generator.default_grants.credits.free'));*/
+
+    // fallback
+    return JSON.parse(await flags.getValue('generator.default_grants.base'));
 }
