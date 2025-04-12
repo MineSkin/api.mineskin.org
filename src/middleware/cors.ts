afbedb58111180a73fd6e51250ed10a75397c1f8
Haylee Schäfer <haylee@inventivetalent.org>
2025-04-12 08:29:46 +0200

    explicitly allow User-Agent header in CORS fixes #392

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors"
 import process from "node:process";
 
-const ALLOWED_HEADERS = ["Content-Type", "Authorization", "MineSkin-User-Agent", "Origin", "Turnstile-Token", "Baggage", "Sentry-Trace"];
+const ALLOWED_HEADERS = ["Content-Type", "Authorization", "User-Agent", "MineSkin-User-Agent", "Origin", "Turnstile-Token", "Baggage", "Sentry-Trace"];
 
 const WEB_WHITELIST = process.env.WEB_CORS!.split(',');
 
