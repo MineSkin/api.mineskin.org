157ed54d0e0219323dd3d2cb8aa57bc2ae8cc6fd
Haylee Schäfer <haylee@inventivetalent.org>
2025-06-04 09:07:09 +0200

    refactor signal forwarding

--- start.sh
+++ start.sh
@@ -4,14 +4,43 @@ echo "[INFO] Starting app..."
 yarn start &
 pid=$!
 
-# Forward signals
-trap "echo '[SIGNAL] Caught SIGHUP'; kill -HUP $pid" HUP
-trap "echo '[SIGNAL] Caught SIGTERM'; kill -TERM $pid" TERM
-trap "echo '[SIGNAL] Caught SIGINT'; kill -INT $pid" INT
 
-# Wait for the process and grab its exit code
-wait $pid
+# Forward signals to the child process
+
+hup() {
+  echo "[SIGNAL] Caught SIGHUP. Forwarding to child ($pid)..."
+  kill -HUP "$pid"
+  wait "$pid"
+  exit_code=$?
+  echo "[INFO] Node exited with code $exit_code"
+  exit $exit_code
+}
+
+terminate() {
+  echo "[SIGNAL] Caught SIGTERM. Forwarding to child ($pid)..."
+  kill -TERM "$pid"
+  wait "$pid"
+  exit_code=$?
+  echo "[INFO] Node exited with code $exit_code"
+  exit $exit_code
+}
+
+interrupt() {
+  echo "[SIGNAL] Caught SIGINT. Forwarding to child ($pid)..."
+  kill -INT "$pid"
+  wait "$pid"
+  exit_code=$?
+  echo "[INFO] Node exited with code $exit_code"
+  exit $exit_code
+}
+
+# Trap signals
+trap hup HUP
+trap terminate TERM
+trap interrupt INT
+
+# Wait for the child normally (if no signal is caught)
+wait "$pid"
 exit_code=$?
-
 echo "[INFO] Node exited with code $exit_code"
 exit $exit_code
