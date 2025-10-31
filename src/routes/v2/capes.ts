25bd1e7d2dab7468b7b32081c280941cc1bf15d1
Haylee Schäfer <haylee@inventivetalent.org>
2025-10-31 11:21:41 +0100

    change /capes cors

--- src/routes/v2/capes.ts
+++ src/routes/v2/capes.ts
@@ -2,13 +2,13 @@ import { Response, Router } from "express";
 import { v2Router } from "./router";
 import expressAsyncHandler from "express-async-handler";
 import { MineSkinV2Request } from "./types";
-import { wildcardCorsWithCredentials } from "../../middleware/cors";
+import { wildcardCors } from "../../middleware/cors";
 import { V2MiscResponseBody } from "../../typings/v2/V2MiscResponseBody";
 import { listKnownCapes } from "../../models/v2/capes";
 import { formatV2Response } from "../../middleware/response";
 
 const router: Router = v2Router();
-router.use(wildcardCorsWithCredentials);
+router.use(wildcardCors);
 
 // alias of /latest
 router.get("/", expressAsyncHandler(async (req: MineSkinV2Request, res: Response<V2MiscResponseBody>) => {
